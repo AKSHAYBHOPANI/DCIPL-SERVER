30b4c8350800570178e4076a571e2f792e4912f6
Archana <archana41.ar@gmail.com>
2021-05-29 10:11:48 +0530

    invest

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const db = knex({
   // Enter your own database information here based on what you created
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'akshaybhopani',
-    password : '',
+    host : 'localhost',
+    user : 'postgres',
+    password : 'Arch@1',
     database : 'dcipl'
   }
 });
