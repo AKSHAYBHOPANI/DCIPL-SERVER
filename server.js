82594baa7b2d27066dce1e7b4b3a6a92103e64d7
Henil <hnkakadiya2705@gmail.com>
2021-07-19 18:28:59 +0530

    updated server.js

--- server.js
+++ server.js
@@ -852,9 +852,41 @@ app.post('/tax', async (req, res) => {
     res.status(200).json(taxData[0]);
   } catch (error) {
     res.status(400).json(error);
-  }
-});
+    async function main() {
+      // Generate test SMTP service account from ethereal.email
+      // Only needed if you don't have a real mail account for testing
 
+
+      // create reusable transporter object using the default SMTP transport
+      let transporter = nodemailer.createTransport({
+        host: "mail.confluence-r.com",
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+          user: '', // generated ethereal user
+          pass: '', // generated ethereal password
+        },
+      });
+
+      // send mail with defined transport object
+      let info = await transporter.sendMail({
+        from: 'akshaybhopani@confluence-r.com', // sender address
+        to: email, // list of receivers
+        subject: `Congratulations ${name}, Your Investment Portfolio Is Generated ✅`, // Subject line
+        html: `<h1>Congratulations ${name}, Your Investment Portfolio Is Generated ✅</h1><h3>You can check your Report on <a href="https://dcipl.yourtechshow.com/features/tax">https://dcipl.yourtechshow.com/features/tax</a> after logging in with your Email ${email}.</h3><p>* This is automated Email sent from DCIPL Server.`, // html body
+      });
+
+      console.log("Message sent: %s", info.messageId);
+      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+      // Preview only available when sending through an Ethereal account
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    }
+
+    main().catch(console.error);
+  }
+})
 app.post('/estate', async (req, res) => {
   try {
     const { name,
